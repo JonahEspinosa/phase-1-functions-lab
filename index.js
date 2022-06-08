f7ef42e2fcd31d66f4edf3da1596917b928e0ac4
Jonah <espinosajonah1245@gmail.com>
2022-06-08 11:44:33 -0400

    commit tree 016ddd22c34206d345a6481269b845bf35ba36b2

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation){
+    const ourlocation = 42
+    return Math.abs(ourlocation - pickupLocation)
+    
+    }
+function distanceFromHqInFeet(pickupLocation){
+    return distanceFromHqInBlocks(pickupLocation)*264;
+}
+function distanceTravelledInFeet(start, destination) {
+const distance = Math.abs(start - destination)*264
+return distance
+}
+function calculatesFarePrice(start, destination){
+  const distanceInFeet = distanceTravelledInFeet(start, destination)
+  if (distanceInFeet>2500){
+  return 'cannot travel that far'
+  }
+  if(distanceInFeet > 2500){
+    return "cannot travel that far";
+}
+
+if(distanceInFeet > 0 && distanceInFeet < 400){
+        return 0;
+}
+
+if(distanceInFeet > 2000 && distanceInFeet < 2500){
+    return 25;
+}
+
+if(distanceInFeet >= 400 && distanceInFeet <= 2000){
+    return ((distanceInFeet-400) * 2) / 100;
+}
+
+}
